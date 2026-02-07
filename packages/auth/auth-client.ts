16e5ab199dd06273bd5704c14aa89f93bf81ce91
Abdul-asa <bigtulahi1@gmail.com>
2026-02-07 01:09:15 +0000

    more url changes

--- packages/auth/auth-client.ts
+++ packages/auth/auth-client.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { createAuthClient } from "better-auth/react";
-import { getWebsiteUrl } from "../env/util";
+import { getWebsiteUrl } from "@/packages/env/client";
 
 export const authClient = createAuthClient({
   baseURL: getWebsiteUrl(),
--- packages/env/client.ts
+++ packages/env/client.ts
@@ -4,3 +4,21 @@ export const env = createEnv({
   client: {},
   runtimeEnv: {},
 });
+
+export function getWebsiteUrl() {
+  if (
+    process.env.VERCEL_ENV === "production" ||
+    process.env.NODE_ENV === "production"
+  ) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; //replace with the actual domain
+  }
+
+  if (
+    process.env.VERCEL_ENV === "preview" ||
+    process.env.VERCEL_ENV === "staging"
+  ) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+
+  return "http://localhost:3000";
+}
