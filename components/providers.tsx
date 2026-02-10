8893e4de7ff87f4a2f5a5cb1d42af73dfd0d1a75
Abdul-asa <bigtulahi1@gmail.com>
2026-02-10 02:04:13 +0000

    more purging

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "@/packages/trpc/client";
-import { ThemeProvider } from "./layout/theme-provider";
 import { Toaster } from "./ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      disableTransitionOnChange
-    >
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+    <>
+      {children}
       <Toaster richColors />
-    </ThemeProvider>
+    </>
   );
 }
