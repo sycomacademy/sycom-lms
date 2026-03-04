86ad60ff0b72dfd45d35941a6be4c91398dd792c
Abdul-asa <bigtulahi1@gmail.com>
2026-03-04 14:12:02 +0000

    f that

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,9 @@
 import type { Config } from "drizzle-kit";
-
-//using process.env.DATABASE_URL for CI workflows
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is required. In CI it is set by the workflowe");
-}
+import { env } from "@/packages/env/server";
 
 export default {
   schema: ["packages/db/helper.ts", "packages/db/schema/*"],
   out: "packages/db/migrations",
   dialect: "postgresql",
-  dbCredentials: { url: databaseUrl },
+  dbCredentials: { url: env.DATABASE_URL },
 } satisfies Config;
