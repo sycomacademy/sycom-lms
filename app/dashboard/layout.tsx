b9d5dc944f25d15c6e5c624f41b09ced4a622a11
Abdul-asa <bigtulahi1@gmail.com>
2026-03-09 11:40:41 +0000

    bto

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,20 @@
 import { cookies } from "next/headers";
 import { DashboardShell } from "@/components/dashboard/dashboard-shell";
 import { HydrateClient, prefetch, trpc } from "@/packages/trpc/server";
+import { getSession } from "@/packages/auth/helper";
+import { redirect } from "next/navigation";
+import { SessionIdentify } from "@/components/auth/session-identify";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await getSession();
+  const session = await getSession();
 
-  // if (!session) {
-  //   redirect("/sign-in");
-  // }
+  if (!session) {
+    redirect("/sign-in");
+  }
 
   const cookieStore = await cookies();
   const sidebarState = cookieStore.get("sidebar_state");
@@ -20,13 +23,13 @@ export default async function DashboardLayout({
 
   return (
     <HydrateClient>
-      {/* <SessionIdentify
+      <SessionIdentify
         user={{
           email: session.user.email,
           id: session.user.id,
           name: session.user.name,
         }}
-      /> */}
+      />
       <DashboardShell defaultOpen={open}>{children}</DashboardShell>
     </HydrateClient>
   );
